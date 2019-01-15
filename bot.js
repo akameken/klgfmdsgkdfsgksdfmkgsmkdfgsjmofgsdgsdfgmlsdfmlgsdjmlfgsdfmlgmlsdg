39ee35f8ec77e076b3d7278aa0c7d61471dff379
dldadnasjndfsjafajnsdfadnfadfkoafasdklk <43988221+dldadnasjndfsjafajnsdfadnfadfkoafasdklk@users.noreply.github.com>
2019-01-15 06:04:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('499247001003884544');
+  const channel = client.channels.get('533855923869974529');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
   if (!size) return channel.setName(`Voice Online : ${currentSize} `);
